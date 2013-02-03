8837877611b63b46a87d6bd0811952dd01060cf0
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-03 18:00:14 +1300

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# 0
+
+<img src=https://secure.travis-ci.org/Dominic Tarr/0.png?branch=master>
+
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,77 @@
+var tape = require('tape')
+var Lock = require('../')
+
+tape('single lock', function (t) {
+
+  var lock = Lock(), r = Math.random(), r2 = Math.random()
+
+  lock('hello', function (release) {
+
+    t.equal(lock.isLocked('hello'), true)
+    release(function (_r, _r2) {
+      t.equal(lock.isLocked('hello'), false)
+      t.equal(_r, r)
+      t.equal(_r2, r2)
+      t.end()
+    })(r, r2)
+  })
+
+})
+
+tape('2 locks', function (t) {
+
+  var lock = Lock(), released = 0
+
+  lock('what?', function (release) {
+    t.equal(lock.isLocked('what?'), true)
+    release(function () {
+      console.log('released!')
+      released = 1
+    })()
+  })
+
+  lock('what?', function (release) {
+    console.log(released)
+    t.equal(released, 1, 'first lock should be completely released')
+    t.equal(lock.isLocked('what?'), true)
+    release(function () {
+      t.equal(lock.isLocked('what?'), false)
+      t.end()
+    })()
+  })
+
+})
+
+tape('3 locks', function (t) {
+
+  var lock = Lock(), released = 0
+
+  lock('what?', function (release) {
+    t.equal(lock.isLocked('what?'), true)
+    release(function () {
+      console.log('released! 1')
+      released = 1
+    })()
+  })
+  lock('what?', function (release) {
+    console.log('1st', released)
+    t.equal(released, 1, 'first lock should be completely released')
+    t.equal(lock.isLocked('what?'), true)
+    release(function () {
+      console.log('released! 2')
+      released = 2
+      t.equal(lock.isLocked('what?'), false)
+    })()
+  })
+  lock('what?', function (release) {
+    console.log('2nd', released)
+    t.equal(released, 2, 'second lock should be completely released')
+    t.equal(lock.isLocked('what?'), true)
+    release(function () {
+      console.log('released! 3')
+      t.equal(lock.isLocked('what?'), false)
+      t.end()
+    })()
+  })
+})
+
